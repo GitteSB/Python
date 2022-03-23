987f5b6a16e12adb2909f77442b2f204e9c5aaf1
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2022-03-23 09:02:58 +0100

    Variables i en lille kafferobot

---
+++ variables.py
@@ -0,0 +1,17 @@
+#Let's start a coffe shop togehter!! We're going to build a coffee shop using som Ptyhon programming concepts
+
+# Let's build a robot Barista
+
+print("Hello, welcome to Nerdi Coffee!!!")
+# Lav en variable som er navnet
+name =input ("What is your name?\n")
+#brug variablen
+print( "Hello " + name + " Thank you so much for coming in today")
+
+menu = "Black coffee, Espresso, Latte, Caramelmochiato"
+
+print( name + " What would you like today? Here's our menu for the day\n" + menu)
+
+order = input()
+
+print("Sounds good " + name + "we'll have that " + order + " ready for you soon")
