28aad553636d2f1636cf6950fc07ea55748f6ce4
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2022-03-23 04:00:54 +0100

    Opstart på at lære Python

---
+++ Examples/Start.py
@@ -0,0 +1,14 @@
+# print en kommandp
+print("I am Iron man")
+
+# Print flere linier
+print("I am Iron man\n","No I am Tony Stark\n","No I am Poppy\n")
+
+# eller i en linen
+print(""" I am Iron man, I am Tony Stark, I am Poppy""")
+# Print på flere linier
+print("""I am Iron man\nI am Tony Stark\nI am Poppy\n""")
+
+# Print flere gange efter hinanden
+
+print("I am Iron Man"* 10)
