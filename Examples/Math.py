3fb621e8e8cd9d40f1c9a292f4a0fdc23b610bca
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2022-03-24 22:14:16 +0100

    New files

---
+++ Examples/Math.py
@@ -0,0 +1,11 @@
+name = " Gitte"
+Age = 7
+#printer de to variables ud med navn og alder
+print ( name, Age)
+# printer typen af variablen ud
+print(type(name))
+# float kommer automatisk, man behøver ikke gøre python opmærksom på det
+actual_age = 46.5
+
+print ( 5 + 7 )
+
--- barista.py
+++ barista.py
@@ -1,7 +1,3 @@
-#Let's start a coffe shop togehter!! We're going to build a coffee shop using som Ptyhon programming concepts
-
-# Let's build a robot Barista
-
 print("Hello, welcome to Nerdi Coffee!!!")
 # Lav en variable som er navnet
 name =input ("What is your name?\n")
@@ -14,4 +10,14 @@ print( name + " What would you like today? Here's our menu for the day\n" + menu
 
 order = input()
 
-print("Sounds good " + name + "we'll have that " + order + " ready for you soon")
+print("Sounds good " + name + " we'll have that " + order + " ready for you soon")
+
+price = 8
+
+number_coffee = input("How many would "+ order + " you like? \n")
+
+total = price * int(number_coffee)
+ 
+print ( total )
+
+
