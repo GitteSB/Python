11f1bb6520dee6617106b815f7504e381150988a
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2022-05-19 11:34:30 +0200

    comments added

--- Webscraper/from selenium.webdriver import Edge.py
+++ Webscraper/from selenium.webdriver import Edge.py
@@ -1,10 +1,9 @@
-from selenium.webdriver import Edge
-driver =Edge(https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+from selenium.webdriver import Edge # first we import the webdriver from Selenium
+driver = Edge(executable_path="C:\Program Files\msedgedriver.exe") #then we get the apropriot driver from our pc 
 
-driver.get('https://oxylabs.io/blog')
+driver.get('https://oxylabs.io/blog') #Then we get the website we want to scrape
 
-blog_title = driver.get_elements_by_css_selector(' h2.blog-card__content-title')
-for title in blog_title:
-    print(title.text)
- driver.quit()# closing the browser
-
+blog_titles = driver.get_elements_by_css_selector(' h2.blog-card__content-title') #What we want to scrape, here it's titles
+for title in blog_titles:
+    print(title.text) # We print the content we scraped for
+driver.quit() # closing the browser
