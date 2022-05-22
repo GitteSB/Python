9e19fce18b00e6dd1d6ba82d54902f1c2c73a3b0
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2022-05-22 20:50:50 +0200

    Exercises from Python book

---
+++ Exercises/Exercise 1-1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+
+
+print(-2)
+
+print(2++2)
+
+print(2, 2)
---
+++ Exercises/Exercise 1-2.py
@@ -0,0 +1,22 @@
+#1
+print("How many seconds are there in 42 minutes 42 seconds?")
+
+x = (42*60)
+y = 42
+result = x+y 
+print(result)
+#2 
+print(" How many miles are there in 10 kilometers?")
+km = 10
+mPrKm = 1.61
+result2 = km/mPrKm
+print(result2)
+
+#3
+print("if you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace ")
+print ("What is your average speed in miles per hour?")
+
+print(((result/60)/result2)/60)
+
+ 
+ 
