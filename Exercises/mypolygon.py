9987ae71ffc1c852a04654c8d888146754635306
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2022-05-25 23:34:41 +0200

    small changes

--- Exercises/mypolygon.py
+++ Exercises/mypolygon.py
@@ -1,9 +1,37 @@
+"""
 import turtle
-sq = turtle.Turtle()
-print(sq)
+import math
+
+firkant = turtle.Turtle()
+print(firkant)
 
 for i in range(4):
-    sq.fd(100)
-    sq.lt(90)
+    firkant.fd(100)
+    firkant.lt(90)
+
+turtle.mainloop()
+"""
+"""
+def polygon(t,n,length):
+    angle = 360/n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+polygon(firkant, 7, 70)
+"""
+import turtle
+import math
+
+bob= turtle.Turtle()
+print(bob)
+
+def circle(t,r):
+    circumference = 2*math.pi * r
+    n = 50
+    length = circumference / n
+    polygon(t, n, length)
+
+
 
 turtle.mainloop()
