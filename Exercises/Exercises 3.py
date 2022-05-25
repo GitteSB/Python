02db40a5e61a53635a7a4031bfedd2eeaefc4d90
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2022-05-25 22:21:46 +0200

    renamed and changes

--- Exercises/Exercises 3.py
+++ Exercises/Exercises 3.py
@@ -1,2 +1,22 @@
-right_justify(s)
-s = "lorem "
+def print_lyrics():
+    print("I'm a lumberjack and I'm okay.")
+    print("I sleep all night and I work all day")
+
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics()
+
+#print(print_lyrics())
+#Exercise 3-2
+
+def do_twice(f):
+    f()
+    f()
+
+def print_spam():
+    print('spam')
+
+do_twice(print_spam)
